48dda1ae29e03923ea4db0ba60ff26f4d4a62508
raoni silla <Raonisilla@gmail.com>
2025-06-10 21:19:34 -0300

    update

--- exercicio01.cpp
+++ exercicio01.cpp
@@ -5,24 +5,60 @@ class aluno {
     private:
         string nome;
         int idade;
-        float nota1, nota2;
+        float n1, n2;
     public:
         aluno(string n, int i, float n1, float n2) {
-            nome = this->nome;
-           
-            nota1 = n1;
-            nota2 = n2;
+            this ->nome = n;
+            this ->idade = i;   
+            this ->n1 = n1;
+            this ->n2 = n2;
+            
         }
 
         void exibirDados() {
             cout << "Nome: " << nome << endl;
             cout << "Idade: " << idade << endl;
-            cout << "Nota 1: " << nota1 << endl;
-            cout << "Nota 2: " << nota2 << endl;
+            cout << "Nota 1: " << n1 << endl;
+            cout << "Nota 2: " << n2 << endl;
         }
 
-        float calcularMedia() {
-            return (nota1 + nota2) / 2;
+        double calcularMedia(double n1, double n2) {
+            float media = this->n1 + this->n2;
+
+           if (media < 60) {
+                cout << "Aluno reprovado!" << endl;
+            } else {
+                cout << "Aluno aprovado!" << endl;
+            }
+
+            return media;
         }
+
+
 
 };
+
+
+int main (){
+    string nome;
+    int idade;
+    float nota1, nota2;
+
+    cout << "Digite o nome do aluno: ";
+    getline(cin, nome);
+    cout << "Digite a idade do aluno: ";
+    cin >> idade;
+    cout << "Digite a nota 1 do aluno: ";
+    cin >> nota1;
+    cout << "Digite a nota 2 do aluno: ";
+    cin >> nota2;
+
+    aluno aluno1(nome, idade, nota1, nota2);
+    
+    aluno1.exibirDados();
+    
+    double media = aluno1.calcularMedia(nota1, nota2);
+    cout << "Média: " << media << endl;
+
+    return 0;
+}
---
+++ exercicio03.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class menorquetres {
+    private:
+    int n1;
+    int n2;
+    int n3;
+    public:
+    menorquetres(int n1, int n2) {
+        this->n1 = n1;
+        this->n2 = n2;
+        this->n3= n3;
+    }
+
+
+    void exibirDados() {
+        cout << "Numero 1: " << n1 << endl;
+        cout << "Numero 2: " << n2 << endl;
+        cout << "Numero 3: " << n3 << endl;
+    }
+
+    int verificarMenor(n1, n2, n3) {
+        int aux = 0;
+        if (n1 < n2 ){
+            aux = n1;
+        } else {
+            aux = n2;
+        }else if ()
+    }
+};
